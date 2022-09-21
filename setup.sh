a656f3bbce930ffc7cd9e480f6839ebe5c330833
Tomiwa-Ot <ot.server1@outlook.com>
2022-09-21 03:27:43 -0700

    updated setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 php-config --extension-dir > /dev/null
 if [ $? -eq 0 ]
 then
-
+	sudo mv Source.so $(php-config --extension-dir)/
 else
-
+	sudo apt install php-config
+	sudo mv Source.so $(php-config --extension-dir)/
 fi
